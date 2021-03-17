cc8f6b4bebc07670d5d37af4ca4ff97180b9c861
thanhdat1499 <thanhdat01041101@gmail.com>
2021-03-17 09:58:10 +0700

    1st upload to GitHub

---
+++ src/se01d/math/util/Se01dMathUtil.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se01d.math.util;
+
+/**
+ *
+ * @author Admin
+ */
+public class Se01dMathUtil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub.");
+        System.out.println("This is 1st upload connects to remote GitHub server");
+    }
+    
+}
